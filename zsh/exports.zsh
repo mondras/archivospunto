ae4195278d6118382fd743436ca29c0e538af0b0
Carlos Mondragon <cadragon@gmail.com>
2015-07-23 00:50:55 +0000

    Update files.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,5 +1,5 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export EDITOR=/usr/bin/vim
-export PATH="./bin:./vendor/local/bin:/home/action/.gem/ruby/2.1.5/bin:/home/action/.parts/packages/ruby2.1/2.1.5/lib/ruby/gems/2.1.0/bin:/home/action/.parts/opt/rubies/ruby-2.1.5/bin:/home/action/.parts/bin:/home/action/.parts/sbin:/home/action/.parts/autoparts/bin:/home/action/.parts/autoparts/bin:/home/action/.parts/autoparts/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/action/.gem/ruby/1.9.1/bin"
+export PATH="./bin:./vendor/local/bin:/home/dev/.gem/ruby/2.1.5/bin:/home/dev/.parts/packages/ruby2.1/2.1.5/lib/ruby/gems/2.1.0/bin:/home/dev/.parts/opt/rubies/ruby-2.1.5/bin:/home/dev/.parts/bin:/home/dev/.parts/sbin:/home/dev/.parts/autoparts/bin:/home/dev/.parts/autoparts/bin:/home/dev/.parts/autoparts/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/dev/.gem/ruby/1.9.1/bin:/home/dev/.meteor"
 
