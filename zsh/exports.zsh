ab249e5a894b694f1d90fc431f9c56f9b3c5cb59
Carlos Mondragon <cadragon@gmail.com>
2016-03-15 21:15:27 +0000

    IUpdates.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,5 +1,5 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-export EDITOR=/usr/bin/vim
+export EDITOR=/home/dev/.parts/bin/vim
 export PATH="./bin:./vendor/local/bin:/home/dev/.gem/ruby/2.1.5/bin:/home/dev/.parts/packages/ruby2.1/2.1.5/lib/ruby/gems/2.1.0/bin:/home/dev/.parts/opt/rubies/ruby-2.1.5/bin:/home/dev/.parts/bin:/home/dev/.parts/sbin:/home/dev/.parts/autoparts/bin:/home/dev/.parts/autoparts/bin:/home/dev/.parts/autoparts/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/dev/.gem/ruby/1.9.1/bin:/home/dev/.meteor:/home/dev/.parts/packages/python2/2.7.6/bin/"
 
