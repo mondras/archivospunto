957809f4d26b1445b3af48fa1718c415829b136b
Carlos Mondragon <cadragon@gmail.com>
2017-01-17 13:48:39 -0800

    Update local.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,4 +1,4 @@
-export PATH=./bin:./vendor/local/bin:/usr/local/bin/:~/.rbenv/shims:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/MacGPG2/bin:/usr/local/bin:/usr/local/sbin/:/bin:/sbin:/usr/bin:/usr/sbin:~/.npm/
+export PATH=./bin:./vendor/local/bin:/usr/local/bin/:~/.rbenv/shims:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/MacGPG2/bin:/usr/local/bin:/usr/local/sbin/:/bin:/sbin:/usr/bin:/usr/sbin:~/.npm/:~/.cargo/env
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export EDITOR=/usr/bin/vim
