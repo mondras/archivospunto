9a8689cc4db1cb06e4b190ef5148878598fcb058
Carlos Mondragon <cadragon@gmail.com>
2017-04-21 21:58:46 +0000

    Updates.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,5 +1,6 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-export EDITOR=/home/dev/.parts/bin/vim
-export PATH="./bin:./vendor/local/bin:/home/dev/.gem/ruby/2.1.5/bin:/home/dev/.parts/packages/ruby2.1/2.1.5/lib/ruby/gems/2.1.0/bin:/home/dev/.parts/opt/rubies/ruby-2.1.5/bin:/home/dev/.parts/bin:/home/dev/.parts/sbin:/home/dev/.parts/autoparts/bin:/home/dev/.parts/autoparts/bin:/home/dev/.parts/autoparts/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/dev/.gem/ruby/1.9.1/bin:/home/dev/.meteor:/home/dev/.parts/packages/python2/2.7.6/bin/"
+export EDITOR=/usr/bin/vim
+export GOPATH=~/dev/go
+export PATH="./bin:./vendor/local/bin:/home/ubuntu/.gem/ruby/2.2.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/dev/.gem/ruby/1.9.1/bin:/home/dev/.meteor:/usr/local/go/bin:/home/ubuntu/.local/bin:/home/ubuntu/dev/go/bin"
 
