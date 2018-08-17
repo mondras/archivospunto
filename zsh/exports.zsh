2bf4e303f3c1723502c79b62274702015d003a6a
Carlos Mondragon <cadragon@gmail.com>
2018-08-17 00:56:33 +0000

    update path.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,5 +2,6 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export EDITOR=/usr/bin/vim
 export GOPATH=~/dev/go
-export PATH="./bin:./vendor/local/bin:/home/ubuntu/.gem/ruby/2.2.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/dev/.gem/ruby/1.9.1/bin:/home/dev/.meteor:/usr/local/go/bin:/home/ubuntu/.local/bin:/home/ubuntu/dev/go/bin"
+export M2_HOME=/opt/maven
+export PATH="./bin:./vendor/local/bin:/home/ubuntu/.gem/ruby/2.2.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/dev/.gem/ruby/1.9.1/bin:/home/dev/.meteor:/usr/local/go/bin:/home/ubuntu/.local/bin:/home/ubuntu/dev/go/bin:${M2_HOME}/bin"
 
