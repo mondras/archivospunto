ebe4bb2462efe580bb5b78ed1a79cb434fed5b16
Carlos Mondragon <cadragon@gmail.com>
2020-02-09 09:05:12 -0800

    Moar updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,7 @@
 alias be='bundle exec'
 alias reload='source ~/.zshrc'
+#neovim
+alias vim="nvim"
+alias vimdiff="nvim -d"
+#config
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,12 +1,27 @@
-export PATH=./bin:./vendor/local/bin:/usr/local/bin/:~/.rbenv/shims:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/MacGPG2/bin:/usr/local/bin:/usr/local/sbin/:/bin:/sbin:/usr/bin:/usr/sbin:~/.npm/:/Applications/SeKey.app/Contents/MacOS:/~/.fastlane/bin:/usr/local/go/bin:~/.local/bin
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-export EDITOR=/usr/bin/vim
-export SSH_AUTH_SOCK=$HOME/.sekey/ssh-agent.ssh
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:/Library/TeX/texbin
+export EDITOR=/usr/local/bin/nvim
+
+export PATH=./bin
+export PATH=$PATH:./vendor/local/bin
+export PATH=$PATH:/usr/local/bin/
+export PATH=$PATH:~/.rbenv/shims
+export PATH=$PATH:/usr/bin
+export PATH=$PATH:/bin
+export PATH=$PATH:/usr/sbin
+export PATH=$PATH:/sbin
+export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:/usr/local/sbin/
+export PATH=$PATH:/sbin
+export PATH=$PATH:~/.npm/
+export PATH=$PATH:/Applications/SeKey.app/Contents/MacOS
+export PATH=$PATH:/~/.fastlane/bin
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:~/.local/bin
 export PATH=$PATH:~/dev/go/bin
-export PATH="$HOME/.fastlane/bin:$PATH"
+export PATH="$HOME/Library/Python/3.7/bin:$PATH"
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+
+export SSH_AUTH_SOCK=$HOME/.sekey/ssh-agent.ssh
 export GOPATH=~/dev/go
